9db7999b260715726b5bb74d55d8bf8af5b8d7bb
Jonas Schubert Erlandsson <more.room@gmail.com>
2015-07-31 17:47:59 +0200

    Fixes warnings in verbose mode.
    Fixes "Instance variable not defined", "shadowing outer local variable" and
    "assigned but unused variable" warnings when using simple_router with rspec and
    petty mode turned on.
    
    The reason for wanting these warnings gone in verbose mode is that they get
    mixed in with the warnings from the application under test and make it hard to
    see what warnings are from the application and what comes from included
    libraries.
    
    This isn't an essential patch, it doesn't fix an error. But it would be nice to
    have :)

--- lib/simple_router.rb
+++ lib/simple_router.rb
@@ -1,7 +1,8 @@
 module SimpleRouter
   class << self
-    attr_accessor :engine
-    def engine() @engine || Engines::SimpleEngine end
+    attr_accessor :engine_reference
+    def engine() self.engine_reference || Engines::SimpleEngine end
+    def engine=(value) self.engine_reference=value end
   end
 
   autoload :DSL,    'simple_router/dsl'
